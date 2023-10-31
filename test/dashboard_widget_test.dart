70cd13f5bc2aa91d3d43246078deb709e8c6f902
George Nunes <george.bentes@gmail.com>
2023-10-30 23:20:08 -0300

    add: enhancement by using the find.byWidgetPredicate function

--- test/dashboard_widget_test.dart
+++ test/dashboard_widget_test.dart
@@ -11,9 +11,23 @@ void main(){
 
   testWidgets('Should display the transfer feature when the Dashboard is opened', (widgetTester) async {
     await widgetTester.pumpWidget(MaterialApp(home: Dashboard()));
-    final iconTransferFeatureItem = find.widgetWithIcon(FeatureItem, Icons.monetization_on);
-    final nameTransferFeatureItem = find.widgetWithText(FeatureItem, "Transfer");
-    expect(iconTransferFeatureItem, findsWidgets);
-    expect(nameTransferFeatureItem, findsOneWidget);
+    final transferFeatureItem = find.byWidgetPredicate((widget) => featureItemMatcher(widget, 'Transfer', Icons.monetization_on));
+    expect(transferFeatureItem, findsOneWidget);
   });
+
+  testWidgets('Should display the transaction feed feature when the Dashboard is opened', (widgetTester) async {
+    await widgetTester.pumpWidget(MaterialApp(home: Dashboard()));
+    final transactionFeedFeatureItem = find
+        .byWidgetPredicate((widget) =>
+        featureItemMatcher(widget, 'Transaction Feed', Icons.description));
+
+    expect(transactionFeedFeatureItem, findsOneWidget);
+  });
+}
+
+bool featureItemMatcher(Widget widget, String name, IconData icon) {
+  if(widget is FeatureItem){
+    return widget.name == name && widget.icon == icon;
+  }
+  return false;
 }
